749d0471c7c4e51916599a24c3f8339ef825219e
Austin Moran <moraustin@gmail.com>
2014-09-07 10:27:55 -0400

    use WebDriverWait instead of Thread.sleep

--- src/com/moraustin/WebDriverTest/GoogleSuggest.java
+++ src/com/moraustin/WebDriverTest/GoogleSuggest.java
@@ -4,18 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class GoogleSuggest {
+
+  private static final String URL = "http://www.google.com/webhp?complete=1&hl=en";
+
   private static final int TIMEOUT = 5000;
-  private static final String RESULTS_DIV = "sbdd_a";
-  private static final String SUGGESTIONS = "//div[@class='sbqs_c']";
+  private static final By RESULTS_DIV = By.className("sbdd_a");
+  private static final By SUGGESTIONS = By.xpath("//div[@class='sbqs_c']");
+  private static final By RESULTS = By.id("rcnt");
+  private static final By BODY = By.tagName("body");
 
   private final WebDriver driver;
+  private final WebDriverWait driverWait;
 
   private GoogleSuggest() {
     this.driver = new FirefoxDriver();
+    this.driverWait = new WebDriverWait(this.driver, TIMEOUT / 1000);
   }
 
   public static void main(String[] args) throws Exception {
@@ -23,58 +33,38 @@ public class GoogleSuggest {
     GoogleSuggest suggest = new GoogleSuggest();
 
     // Go to the Google Suggest home page
-    suggest.driver.get("http://www.google.com/webhp?complete=1&hl=en");
-
+    suggest.driver.get(URL);
 
     // Enter the query string "Cheese"
-    WebElement query = suggest.waitForDisplayedElement(By.name("q"), TIMEOUT);
+    WebElement query = suggest.driver.findElement(By.name("q"));
     query.sendKeys("Cheese");
 
-    Thread.sleep(500);
+    suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS_DIV));
 
-    suggest.waitForDisplayedElement(By.className(RESULTS_DIV), TIMEOUT);
+    suggest.until((WebDriver input) -> input.findElements(SUGGESTIONS).size() > 0);
 
-    // And now list the suggestions
+    List<WebElement> allSuggestions = suggest.driver.findElements(SUGGESTIONS);
 
-    List<WebElement> allSuggestions = suggest.waitForDisplayedElements(By.xpath(SUGGESTIONS), TIMEOUT);
-
+    // List the suggestions
     for (WebElement suggestion : allSuggestions) {
       System.out.println(suggestion.getText());
     }
 
-    Thread.sleep(500);
+    suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS));
 
     // Now list the visible text
-    WebElement body = suggest.driver.findElement(By.tagName("body"));
+    WebElement body = suggest.driver.findElement(BODY);
     System.out.println(body.getText());
 
     suggest.driver.quit();
   }
 
-  private WebElement waitForDisplayedElement(By by, int timeout) {
-    long end = System.currentTimeMillis() + timeout;
-    while (System.currentTimeMillis() < end) {
-      WebElement retval = driver.findElement(by);
-
-      // If results have been returned, the results are displayed in a drop down.
-      if (retval.isDisplayed()) {
-        return retval;
-      }
-    }
-    return null;
+  private void until(final com.google.common.base.Predicate<WebDriver> isTrue) {
+    driverWait.until(isTrue);
   }
 
-  private List<WebElement> waitForDisplayedElements(By by, int timeout) {
-    long end = System.currentTimeMillis() + timeout;
-    while (System.currentTimeMillis() < end) {
-      List<WebElement> retval = driver.findElements(by);
-
-      // If results have been returned, the results are displayed in a drop down.
-      if (retval != null) {
-        return retval;
-      }
-    }
-    return null;
+  private void until(final ExpectedCondition<WebElement> isTrue) {
+    driverWait.until(isTrue);
   }
 }
 
