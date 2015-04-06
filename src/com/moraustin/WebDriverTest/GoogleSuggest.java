8168a5f2f1c4ec277f7afc377fac1020a5c8990c
Austin Moran <austin.moran@surveysampling.com>
2015-04-05 22:52:12 -0400

    update dependencies, test remote webdriver

--- src/com/moraustin/WebDriverTest/GoogleSuggest.java
+++ src/com/moraustin/WebDriverTest/GoogleSuggest.java
@@ -3,68 +3,72 @@ package com.moraustin.WebDriverTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class GoogleSuggest {
 
-  private static final String URL = "http://www.google.com/webhp?complete=1&hl=en";
+    private static final String TARGET_URL = "http://www.google.com/webhp?complete=1&hl=en";
 
-  private static final int TIMEOUT = 5000;
-  private static final By RESULTS_DIV = By.className("sbdd_a");
-  private static final By SUGGESTIONS = By.xpath("//div[@class='sbqs_c']");
-  private static final By RESULTS = By.id("rcnt");
-  private static final By BODY = By.tagName("body");
+    private static final int TIMEOUT = 5000;
+    private static final By RESULTS_DIV = By.className("sbdd_a");
+    private static final By SUGGESTIONS = By.xpath("//div[@class='sbqs_c']");
+    private static final By RESULTS = By.id("rcnt");
+    private static final By BODY = By.tagName("body");
 
-  private final WebDriver driver;
-  private final WebDriverWait driverWait;
+    private final WebDriver driver;
+    private final WebDriverWait driverWait;
 
-  private GoogleSuggest() {
-    this.driver = new FirefoxDriver();
-    this.driverWait = new WebDriverWait(this.driver, TIMEOUT / 1000);
-  }
-
-  public static void main(String[] args) throws Exception {
-
-    GoogleSuggest suggest = new GoogleSuggest();
-
-    // Go to the Google Suggest home page
-    suggest.driver.get(URL);
-
-    // Enter the query string "Cheese"
-    WebElement query = suggest.driver.findElement(By.name("q"));
-    query.sendKeys("Cheese");
-
-    suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS_DIV));
-
-    suggest.until((WebDriver input) -> input.findElements(SUGGESTIONS).size() > 0);
-
-    List<WebElement> allSuggestions = suggest.driver.findElements(SUGGESTIONS);
-
-    // List the suggestions
-    for (WebElement suggestion : allSuggestions) {
-      System.out.println(suggestion.getText());
+    private GoogleSuggest() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.firefox();
+        this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+        this.driverWait = new WebDriverWait(this.driver, TIMEOUT / 1000);
     }
 
-    suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS));
+    public static void main(String[] args) throws Exception {
 
-    // Now list the visible text
-    WebElement body = suggest.driver.findElement(BODY);
-    System.out.println(body.getText());
+        GoogleSuggest suggest = new GoogleSuggest();
 
-    suggest.driver.quit();
-  }
+        // Go to the Google Suggest home page
+        suggest.driver.get(TARGET_URL);
 
-  private void until(final com.google.common.base.Predicate<WebDriver> isTrue) {
-    driverWait.until(isTrue);
-  }
+        // Enter the query string "Cheese"
+        WebElement query = suggest.driver.findElement(By.name("q"));
+        query.sendKeys("Cheese");
 
-  private void until(final ExpectedCondition<WebElement> isTrue) {
-    driverWait.until(isTrue);
-  }
+        suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS_DIV));
+
+        suggest.until((WebDriver input) -> input.findElements(SUGGESTIONS).size() > 0);
+
+        List<WebElement> allSuggestions = suggest.driver.findElements(SUGGESTIONS);
+
+        // List the suggestions
+        for (WebElement suggestion : allSuggestions) {
+            System.out.println(suggestion.getText());
+        }
+
+        suggest.until(ExpectedConditions.visibilityOfElementLocated(RESULTS));
+
+        // Now list the visible text
+        WebElement body = suggest.driver.findElement(BODY);
+        System.out.println(body.getText());
+
+        suggest.driver.quit();
+    }
+
+    private void until(final com.google.common.base.Predicate<WebDriver> isTrue) {
+        driverWait.until(isTrue);
+    }
+
+    private void until(final ExpectedCondition<WebElement> isTrue) {
+        driverWait.until(isTrue);
+    }
 }
 
